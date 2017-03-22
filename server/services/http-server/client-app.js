5a253fa47f14ebe32e6411a889bf43411431b97f
Mariusz Nowak <medyk@medikoo.com>
2017-03-22 17:21:34 +0100

    fix: prevent fouc effect
    The SiteTree renderer loads head and body content at once.
    So if styles are not loaded with initial HTML we have fouc effect.
    This workarounds that.
    Problem is described at https://github.com/medikoo/site-tree/issues/4

--- server/services/http-server/client-app.js
+++ server/services/http-server/client-app.js
@@ -13,5 +13,7 @@ module.exports = function (req, res, next) {
 	res.setHeader("X-UA-Compatible", "IE=edge,chrome=1");
 	res.setHeader("Content-Type", "text/html; charset=utf-8");
 	res.setHeader("Cache-Control", "max-age=365000000, immutable");
-	res.end(`<!DOCTYPE html><script src="${ env.assetsRoot }main.js"></script>`);
+	res.end(`<!DOCTYPE html>
+	<link href="${ env.assetsRoot }style.css" rel="stylesheet" />
+	<script src="${ env.assetsRoot }main.js"></script>`);
 };
