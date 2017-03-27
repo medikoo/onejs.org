f07864e7491dab2af895f5687dd5973a32129d00
Mariusz Nowak <medyk@medikoo.com>
2017-03-27 11:28:56 +0200

    refactor: update package view after recent changes

--- view/package.js
+++ view/package.js
@@ -1,20 +1,17 @@
 "use strict";
 
-const { a, aside, div, footer, h1, header, main, p, section } = require("../services/domjs").ns;
+const { a, aside, h1, p, section } = require("../services/domjs").ns;
 
 exports._parent = require("./base");
 exports._match = "name";
 
-exports.body = function () {
-	header(div({ class: "content" },
-		h1(this.name),
-		p({ class: "github" }, a({ href: `https://github.com/${ this.githubPath }` }, "GitHub")),
-		p({ class: "version" }, `v${ this.version }`)));
+exports.header = function () {
+	h1(this.name);
+	p({ class: "github" }, a({ href: `https://github.com/${ this.githubPath }` }, "GitHub"));
+	p({ class: "version" }, `v${ this.version }`);
+};
 
-	main({ class: "content" },
-		aside("Aside"),
-		section("Main section")
-	);
-
-	footer(div({ class: "content" }, a({ href: "/" }, "onejs.org")));
+exports.main = function () {
+	aside("Aside");
+	section("Main section");
 };
