f876dcbafd0cce0eda6b59d67543d73910732f5e
Mariusz Nowak <medyk@medikoo.com>
2017-03-28 13:23:25 +0200

    feat: improve header style

--- view/package.js
+++ view/package.js
@@ -2,15 +2,19 @@
 
 "use strict";
 
-const { a, aside, h1, p, section } = require("../services/domjs").ns;
+const { assetsRoot } = require("../services/env");
+
+const { a, aside, h1, img, p, section } = require("../services/domjs").ns;
 
 exports._parent = require("./base");
 exports._match = "name";
 
 exports.header = function () {
 	h1(this.name);
-	p({ class: "github" }, a({ href: `https://github.com/${ this.githubPath }` }, "GitHub"));
-	p({ class: "version" }, `v${ this.version }`);
+	p({ class: "header-version" }, `@  ${ this.version }`);
+	p({ class: "header-repo-link" },
+		a({ href: `https://github.com/${ this.githubPath }` },
+			img({ src: `${ assetsRoot }logo-github.svg`, alt: "GitHub" })));
 };
 
 exports.main = function () {
