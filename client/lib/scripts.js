9c98d9a86e5f9a5cd7e2944d3dbf1eae9c375d27
Mariusz Nowak <medyk@medikoo.com>
2017-04-04 17:19:13 +0200

    feat: force scripted target focus in non SPA mode

--- client/lib/scripts.js
+++ client/lib/scripts.js
@@ -1,7 +1,10 @@
-// Extra scripts that aid experience
+// Extra scripts that aid UX
 // Should be loaded also not in SPA mode
 
 "use strict";
 
 require("../services/top-heading-hash-linker")({ contextSelector: ".markdown" });
 require("../services/hash-to-ahref-linker");
+
+// We force scripted target focus as it doesn't work well natively in non SPA mode
+require("../services/on-click-hash-focus-target");
