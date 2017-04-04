ea92bbecc2e2eed067642703d7f75b5d007c6bc1
Mariusz Nowak <medyk@medikoo.com>
2017-04-04 12:04:10 +0200

    feat: ensure headers reload after new page render

--- client/processes/spa.js
+++ client/processes/spa.js
@@ -9,6 +9,12 @@ require("../lib/setup-env-service.generated")("spa");
 require("../lib/setup-package-meta-resolver");
 require("../services/location-to-history");
 require("../services/on-click-hash-focus-target");
-require("../services/history-to-view-router");
+const historyViewRouter = require("../services/history-to-view-router");
 
 require("../lib/scripts");
+const topHeadingHashLinker = require("../services/top-heading-hash-linker");
+
+// Additional bindings between services:
+
+// Reload headings after new page is loaded
+historyViewRouter.on("load", topHeadingHashLinker.reload);
