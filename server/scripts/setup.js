246177c4702d77be492b7cb851a2bcdeabee920d
Mariusz Nowak <medyk@medikoo.com>
2017-04-05 09:18:05 +0200

    feat: generate css and js only when production env

--- server/scripts/setup.js
+++ server/scripts/setup.js
@@ -2,15 +2,18 @@
 
 "use strict";
 
+require("../lib/setup-env-service");
+
 const deferred         = require("deferred")
     , resolveClientEnv = require("./generate-client-env-resolver")
     , generateCss      = require("./generate-css")
-    , generateJs       = require("./generate-js");
+    , generateJs       = require("./generate-js")
+    , { env }          = require("../../services/env");
 
 module.exports = () => deferred(
 	resolveClientEnv(),
-	generateCss(),
-	generateJs()
+	(env === "production") && generateCss(),
+	(env === "production") && generateJs()
 );
 
 if (require.main === module) module.exports().done();
